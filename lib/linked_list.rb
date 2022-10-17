c8fd0574ad5fe7be20af861069273a15135121ac
Olly Seber <olly_s@hotmail.co.uk>
2022-10-17 14:44:17 -0700

    Add append function to LinkedList

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,13 +1,32 @@
 # frozen_string_literal: true
 
 class LinkedList
+  attr_reader :head, :tail
 
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+
+  def append(value)
+    node = Node.new(value)
+    if empty?
+      @head = node
+      @tail = node
+    else
+      @tail.next_node = node
+      @tail = next_node
+  end
+
+  def empty?
+    head.nil?
+  end
 end
 
 class Node
   attr_accessor :value, :next_node
 
-  def initialize
+  def initialize(value)
     @value = nil
     @next_node = nil
   end
